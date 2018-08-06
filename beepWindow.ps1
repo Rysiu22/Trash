6881fdd7fd07978819427dc39f940ad90a6b6b0b
Rysiu22 <rys1u22@wp.pl>
2018-08-06 23:51:37 +0200

    Create beepWindow.ps1

---
+++ beepWindow.ps1
@@ -0,0 +1,14 @@
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms");
+
+while(1 -gt 0)
+{
+    $a = Get-Process |where {$_.mainWindowTitle -and ($_.mainWindowTitle -like "*błąd*" -or $_.mainWindowTitle -like "*komunikat [1-9]*")};
+    
+    $a | format-table id,name,mainwindowtitle –AutoSize;
+    if($a.count -gt 0)
+    {
+        [console]::beep(1000,500)
+        [System.Windows.forms.MessageBox]::Show('Run?','Game input','OK','Error')
+    };
+    Start-Sleep -s 10
+}
